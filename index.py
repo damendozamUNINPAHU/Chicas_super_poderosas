4d6e2e34097880a3ad0253bbf6f0bdae965f8d2a
lmunozgo02 <lmunozgo02@uninpahu.edu.co>
2020-11-11 23:09:23 -0500

    se realizo bienvendia y ajuste de bollean con triage y temperatura

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
-userId=input ()
+print ("bienvenido supersalud Lauger") #se agrego bienvenida
+userId=input ("ingresa tu nombre ")
 print(userId)
-birthDate=input()
+birthDate=input("ingresa tu fecha de nacimiento ")
 print(birthDate)
-
-userId 
-    print "Geraldine"  
-        if True: 
-            print ("usuario activo")
-        else:
-            print ("Usuario inactivo")
+temperature= int(input("ingresa tu temperatura ")) #se adiciona boleean basico para división de triage
+if temperature >= 34:
+    if temperature < 37:
+        print(f"Su temperatura es {temperature}")
+        print("Triage 3")
+    elif temperature >= 37:
+        print(f"Su temperatura es {temperature}")
+        print("Triage 1")
+else:
+    print("por favor ingrese nuevamente los datos")
